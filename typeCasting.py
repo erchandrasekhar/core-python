7f81c644dcb2d72d191776f98c512b4e4a16c40a
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-08-31 20:21:05 +0530

    added type casting in python

---
+++ typeCasting.py
@@ -0,0 +1,12 @@
+
+x = 5.6
+y = 5.2
+p = 5
+z ="chandrasekhar yadav"
+print(type(x))
+print(type(y))
+print(type(z))
+
+print("here x is typecasting fload to int",int(x))
+print("here int is casting as float",float(p))
+print("x is casting as string",str(p))
