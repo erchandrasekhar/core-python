08795a370838a57d38a35bf96daf15e3bae55d5b
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-08-31 22:26:38 +0530

    airtmatic oprator code added

--- Python Operators.py
+++ Python Operators.py
@@ -2,6 +2,15 @@
 print("######################### Arithmetic operators ###########################")
 x = 16
 y = 5
+print("addition of two number      :",x+y)
+print("substation of two number    :",x-y)
+print("division of two number      :",x/y)
+print("multiplication of two number:",x*y)
+print("modulus of two number       :",x%y)
+
+#Python Assignment Operators
+print("###################### Python Assignment Operators ####################")
+
 
 
 
