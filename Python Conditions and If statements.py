362c91c6aed127cb9c2ab49057caee34d4099ba7
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-08-31 20:07:03 +0530

    more if and code added

--- Python Conditions and If statements.py
+++ Python Conditions and If statements.py
@@ -1,4 +1,6 @@
 
+# first code 1
+print("##############First code output ################################################")
 x = 20
 y = 13
 
@@ -8,3 +10,24 @@ elif x > y:
     print("x is greater than y")
 else:
     print("y is grater than x")
+
+# second code
+#Short Hand If ... Else
+print("######################### second code out put ###############################")
+a = 20
+b = 60
+print("b  is greater than b") if b > a else print("a is greater than b")
+
+# third code
+# and and conditional operator
+print("######################## Third code output ###########################")
+
+a = 2088
+b = 568
+c = 90
+if a > b and a > c:
+    print("A is greater than both a and b")
+elif b > c and b > a:
+    print("b is Greater than a and c")
+else:
+    print("c is greater than b")
